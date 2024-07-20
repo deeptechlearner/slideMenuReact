87625ce954bf527f71088dd85a089df4115b459e
Venkat <venkatdeeptechlearner@gmail.com>
2024-07-20 19:05:17 +0530

    using PureComponent instead of shouldComponentUpdate

--- src/MenuButton.js
+++ src/MenuButton.js
@@ -1,14 +1,8 @@
-import React, { Component } from "react";
+import React, { PureComponent  } from "react";
 import './MenuButton.css';
  
-class MenuButton extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    if (nextProps.handleMouseDown === this.props.handleMouseDown) {
-      return false;
-    } else {
-      return true;
-    }
-  } 
+class MenuButton extends PureComponent  {
+  
   render() {
     console.log("Rendering: MenuButton");
     return (
